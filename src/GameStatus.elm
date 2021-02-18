5e5f073eeeb4cac8ac169240870b52274abc00da
Arjun Aravind <arjun.aravind1998@gmail.com>
2021-02-18 21:28:31 +0530

    Add more functions to calculate win

--- src/GameStatus.elm
+++ src/GameStatus.elm
@@ -36,3 +36,39 @@ anyPlayerHasWonHorizontally board rowNumber colNumber currentPlayerNumber =
             anyPlayerHasWonHorizontally board rowNumber (colNumber-1) currentPlayerNumber
     else
         anyPlayerHasWonHorizontally board (rowNumber-1) 2 currentPlayerNumber
+
+anyPlayerHasWonVertically : Board -> Int -> Int -> Int -> Bool
+anyPlayerHasWonVertically board rowNumber colNumber currentPlayerNumber =
+    if (colNumber < 0) then
+        if (currentPlayerNumber == 0) then
+            False
+        else
+            anyPlayerHasWonVertically board 2 2 (currentPlayerNumber-1)
+    else if(rowNumber < 0) then
+        True
+    else if ((getCellValueFromBoard rowNumber colNumber board) == currentPlayerNumber) then
+        if (currentPlayerNumber == 0) then
+            False
+        else
+            anyPlayerHasWonVertically board (rowNumber-1) colNumber currentPlayerNumber
+    else
+        anyPlayerHasWonVertically board 2 (colNumber-1) currentPlayerNumber
+
+
+anyPlayerHasWonDiagonally : Board -> Int -> Bool
+anyPlayerHasWonDiagonally board currentPlayerNumber =
+    let
+        bottomLeft = ((getCellValueFromBoard 2 0 board) == currentPlayerNumber)
+        topLeft = ((getCellValueFromBoard 0 0 board) == currentPlayerNumber)
+        center = ((getCellValueFromBoard 1 1 board) == currentPlayerNumber)
+        topRight = ((getCellValueFromBoard 0 2 board) == currentPlayerNumber)
+        bottomRight = ((getCellValueFromBoard 2 2 board) == currentPlayerNumber)
+    in
+    if (currentPlayerNumber == 0) then
+        False
+    else if ( bottomLeft && center && topRight ) then
+        True
+    else if ( bottomRight && center && topLeft ) then
+        True
+    else
+        anyPlayerHasWonDiagonally board (currentPlayerNumber-1)
